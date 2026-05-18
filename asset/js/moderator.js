a299b6b24c1a688f0bc7d4e4ca2e4f76202c32e6
Tammanna Billah <tamannabillah23@gamil.com>
2026-05-18 11:23:16 +0600

    sent http request to add_content_check.php

--- asset/js/moderator.js
+++ asset/js/moderator.js
@@ -3,21 +3,29 @@ function uploadCheck(){
     let form = document.getElementById("contentForm");
     let data = new FormData(form);
 
-    fetch("../../controller/Moderator/add_content_check.php",{
-        method:"POST",
-        body:data
-    })
-    .then(res => res.json())
-    .then(res => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "../../controller/Moderator/add_content_check.php", true);
 
-        alert(res.message);
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState !== 4) return;
 
-        if(res.success){
-            form.submit();
+        if(xhr.status === 200){
+            try {
+                let res = JSON.parse(xhr.responseText);
+                alert(res.message);
+
+                if(res.success){
+                    form.submit();
+                }
+            } catch(e) {
+                alert("Invalid server response.");
+                console.error(e, xhr.responseText);
+            }
+        } else {
+            alert("Something went wrong");
+            console.error("AJAX error", xhr.status, xhr.statusText, xhr.responseText);
         }
-    })
-    .catch(err => {
-        alert("Something went wrong");
-        console.log(err);
-    });
+    };
+
+    xhr.send(data);
 }
